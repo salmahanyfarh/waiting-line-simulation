5832149604cc2df0ba4cc53c14df310c7016921c
salma hany <51351106+salmahanyfarh@users.noreply.github.com>
2020-06-08 15:00:52 +0200

    Update WaitingLineSimulation.java

--- src/waitinglinesimulation/WaitingLineSimulation.java
+++ src/waitinglinesimulation/WaitingLineSimulation.java
@@ -20,48 +20,7 @@ public class WaitingLineSimulation {
         
         GuiOfModel x = new GuiOfModel();
         x.setVisible(true);
-         System.out.println(" Welcome to WAITING LINE MODEL \n\n");
-         float arrivalNumbers , servedNumber ;
-         int i = 0 ; 
-         Scanner scanf = new Scanner(System.in);
-         float [][]Customers = new float[i][2];
-         System.out.println("Please enter the number of arrival per hour :\n");
-         arrivalNumbers= scanf.nextInt();
-        System.out.println("Please enter the number of customers or unit served per hour :\n");
-        servedNumber = scanf.nextInt();
-        System.out.println("\nPlease Enter Case To Solve By :\n") ;
-       
-       
-    Rules L = new Rules();
-    L.avr_customers_of_system(arrivalNumbers, servedNumber);
-     
-    System.out.println("\n\nThe average number of customers or units in the system : \n%f\n\n"+L) ;
-     Rules W = new Rules();
-     W.spending_avr_time(arrivalNumbers,servedNumber);
-    System.out.println("The average time a customer spends in the system : \nHours : %f  "+W) ;
-    System.out.println("The average time a customer spends in the system : \nmin : %f  "+ 60*W) ;
-
-    Rules Lq = new Rules();
-    Lq.avr_customers_in_queue(arrivalNumbers,servedNumber);
-    System.out.println("The average number of customers in the queue : \n%f\n\n" + Lq) ;
-    Rules Wq = new Rules(); 
-    Wq.spending_avr_time_queue(arrivalNumbers,servedNumber);
-    System.out.println("hours" + Wq);
-    System.out.println("The average time a customer spends waiting in the queue   Minutes : %f\n\n"+ ""+Wq*60);
-     Rules U_F = new Rules(); // it is called utilization factor or rho (p)
-    U_F.utilization_factor(arrivalNumbers,servedNumber);
-    System.out.println("Utilization factor : \n%f\n\n"+U_F) ;
-     Rules P0 = new Rules(); 
-    P0.idle_time(arrivalNumbers,servedNumber);
-    System.out.println("The percent idle time, P0, or the probability no one is in the system : \n%f\n\n"+P0) ;
-   System.out.println("How many probabilities that you want to test ? :\n") ;
-    int num ;
-    num = scanf.nextInt() ;
-    float [][]customers = new float[num][2];
-    Rules compare = new Rules();
-    compare.Customers_greaterthan_N(num,arrivalNumbers,servedNumber,customers);
-    System.out.println("\n\nk          Pn>k= (arrivals_no/Served_no)^k+1") ;
-    
+        
     
     }   
 }
